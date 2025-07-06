7bae7ddce2c30c2d503e95f6e9af8b0951dbeb6b
Abhishek Singh <abhi31m1@gmail.com>
2025-07-06 17:27:12 +0530

    minor changes

--- part1/main.py
+++ part1/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from schemas import PromptRequest, PromptResponse
+from .schemas import PromptRequest, PromptResponse
 from agent import run_agent
 
 app = FastAPI(
